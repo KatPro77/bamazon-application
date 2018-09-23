c9e5fe436a418ad5ee68f3caf6e0336cb4736df6
Katerina Stanley <katerinastanley77@gmail.com>
2018-09-23 13:35:38 -0400

    add db file

---
+++ bamazon_schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products(
+	item_id INTEGER AUTO_INCREMENT NOT NULL,
+	product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+	stock_quantity INTEGER(10) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+SELECT * from products;
---
+++ seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES
+('Nikon D5', 'Cameras', 6500, 50),
+('Nikon D750', 'Cameras', 1900, 150),
+('GoPro Hero7', 'Cameras', 400, 100),
+('Camera Bag', 'Camera-Accessories', 75, 30),
+('Tripod', 'Camera-Accessories', 60, 200),
+('32GB Memory Card', 'Camera-Accessories', 30, 500),
+('Wireless Remote Control', 'Camera-Accessories', 15, 70),
+('Nikon Rechargable Battery', 'Camera-Accessories', 40, 25),
+('400mm Telephoto Lens','Lenses', 2000, 25),
+('14-24mm Wide Lens', 'Lenses', 1250, 10),
+('HD 52mm Lens Filter', 'Lenses', 8, 65),
+('52mm UV Lens Filter Kit', 'Lenses', 33, 123)
